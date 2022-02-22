a470051736f62f70936d9b4f6d5e72c3e4a78269
Jens Preussner <jnz+github@posteo.de>
2022-02-22 16:40:33 +0100

    Fixed bug in extra dict

--- superdesk/io/feed_parsers/f3json.py
+++ superdesk/io/feed_parsers/f3json.py
@@ -70,9 +70,9 @@ class F3JSONFeedParser(FeedParser):
         item["priority"] = int(article.get("priority", "5"))
 
         item["extra"] = {
-            "factsnack-claim" = article.get("claim", "")
-            "factsnack-fact" = article.get("fact", "")
-            "factsnack-source" = article.get("uri")
+            "factsnack-claim": article.get("claim", "")
+            "factsnack-fact": article.get("fact", "")
+            "factsnack-source": article.get("uri")
         }
 
         return item
